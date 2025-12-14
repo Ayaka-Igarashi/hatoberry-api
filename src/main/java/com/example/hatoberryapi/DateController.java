ce30f583b609ddcab8d90a55cc815e6728529251
Ayaka-Igarashi <ayakaiga86@gmail.com>
2025-12-14 13:43:21 +0900

    Initial commit

---
+++ src/main/java/com/example/hatoberryapi/DateController.java
@@ -0,0 +1,17 @@
+package com.example.hatoberryapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class DateController {
+    @GetMapping("/date")
+    public String getDate() {
+        LocalDateTime nowDate = LocalDateTime.now();
+        DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss.SSS");
+        return dtf1.format(nowDate);
+    }
+}
---
+++ src/main/java/com/example/hatoberryapi/HatoberryApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hatoberryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatoberryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HatoberryApiApplication.class, args);
+	}
+
+}
